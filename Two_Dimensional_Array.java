e5e21cf0e4fb69e85c1320332054bbd54b44f880
Khalid B <skhalid90712@gmail.com>
2024-10-29 11:55:04 +0530

    Update Two_Dimensional_Array.java

--- Two_Dimensional_Array.java
+++ Two_Dimensional_Array.java
@@ -2,117 +2,75 @@ package Arrays;
 
 import java.util.Arrays;
 
-public class Two_Dimensional_Array {
-
+public class Single_Dimensional_Array {
+	
 	public static void main(String[] args) {
-		
-		/* 1) Declaring an Array
-		 * 2) Initializing an Array
-		 * 3) Finding length/size of an Array
-		 * 4) Accessing elements of an Array - Single elements via Indexing
-		 *                                   - All the elements using forloop and Enhanced for-Each loop.
-		 *                                   - All data elements at once using Methods
-		 * */
-		
-		
-		/**************1) Two different Approaches of Declaring an Array**************/
+    
+    /**************1) Two different Approaches of Declaring an Array**************/
+    
+      //Static declaration - Preferred, if the size of array is known.
 			
-			//Static declaration - Preferred, if the size of array is known.
-		
-				int Numbers[][] = new int[3][3]; 
+		      int numbers[] = new int[3];  //Initialized in Line Number 25 to 27
 				
 			//Dynamic Declaration and Initialization - Used, if the size of the array is uncertain or unknown
+			
+		      String Levels_Of_Testings[] = {"Unit","System","Integration","UserAcceptance"}; 
 				
-				String[][] Series = { {"A","B","C"}, {"D","E","F"}, {"G","H","I"}, {"J","K","L"} };
-				
-				
-				
-		/**************2) Initializing - Static Array Variable - declared above in Line number 22 **************/
-				
-				Numbers[0][0]= 1;
-				Numbers[0][1]= 2;
-				Numbers[0][2]= 3;
-				
-				Numbers[1][0]= 4;
-				Numbers[1][1]= 5;
-				Numbers[1][2]= 6;
-				
-				Numbers[2][0]= 7;
-				Numbers[2][1]= 8;
-				Numbers[2][2]= 9;
-				
-				
-				
-		/**************3) Finding Length of an Array Using Length Attribute**************/
-				
-				/*Every array in Java has a property called length, which holds the number of elements in that array.
-				 *The length attribute is not a method (no parentheses are needed)
-				 *To find the length of a two-dimensional array in Java - Finding number of rows : Using length property of an array, find the length of the main array itself.
-				 *                                                      - Finding number of columns : find the length of each row to find the number of columns in that row using array length property*/
-
-				
-				
-				//Array Variable(Numbers) has been declared in Line 22 and Initialized in Line 32 through LIne 42.
-				
-				System.out.println("Number of rows: "+ Numbers.length);
-				System.out.println("Number of Colums: "+ Numbers[0].length);
-				
-				
-				
-				
-	   /**************4) Reading/Accessing of Array Data Elements**************/
-				
-				
-				//Accessing single data element using Array Indexing:
-				
-					int LastRowFirstColumn = Numbers[2][0];
-				
-					System.out.println("Data Element at index [2][0] Position of Array(Numbers): "+LastRowFirstColumn);
-					
-					String LastRowLastColumn = Series[3][2];
-					
-					System.out.println("Data Element at index [3][2] Position of Array(Series): "+LastRowLastColumn);
-					
-					
-			  //Accessing all array elements - Using Normal forLoop: Array(Numbers) is declared in Line number 22  and initialized in Line number 32 to 42.
-					
-					System.out.println("Data Elements of Two Dimension Array(Number) using for loop: ");
-					for(int i=0;i<Numbers.length;i++) {
-						for(int j=0;j<Numbers[i].length;j++) {
-							System.out.print(Numbers[i][j]+" ");	
-						}
-						System.out.println();
-					} 
-					
-					System.out.println("Data Elements of Two Dimension Array(Series) using for loop: ");
-					for(int i=0;i<Series.length;i++) {
-						for(int j=0;j<Series[i].length;j++) {
-							System.out.print(Series[i][j]+" ");
-						}
-						System.out.println();
-					}
-					
-					
-			  //Accessing all array elements - Using Enhanced for-each Loop: Array(numbers) is declared in Line number 22 and initialized in Line number 32 to 42.
-					
-					
-					System.out.println("Data Elements of Two Dimension Array(Number) using Enhanced Loop: ");
-					for(int row[]:Numbers) {
-						for(int x:row) {
-							System.out.print(x+" ");
-						}
-						System.out.println();
+			
+			
+    /**************2) Initializing - Static Array Variable - declared above in Line number 13 **************/
+    
+			  numbers[0]=10;
+				numbers[1]=20;
+				numbers[2]=30;
 						
-					}
+    /**************3) Finding Length of an Array Using Length Attribute**************/
+				
+				//Every array in Java has a property called length, which holds the number of elements in that array.
+				//The length attribute is not a method (no parentheses are needed)
+				
+					System.out.println("Length/Size of Array(numbers):"+numbers.length);
+				
+				
+    /**************4) Reading/Accessing of Array Data Elements**************/
+				
+		      //Accessing single data element using Array Indexing:
+				 
+					    int LastElement = numbers[2]; // Can also be written as: int LastElement = numbers[numbers.length-1];
+					
+					    System.out.println("Data Element at 2nd Index Position in an Array(numbers): "+LastElement);
+    
+				
+				  //Accessing all array elements - Using Normal forLoop: Array(numbers) is declared in Line number 13 and initialized in Line number 25 to 27
+				
+					    System.out.println("Data Elements of Array(numbers) using for loop:");
+					    for(int i=0; i<numbers.length; i++) {
+					      System.out.println(numbers[i]);	
+                
+					    }
+				
+				//Accessing all array elements - Using Enhanced for-each Loop: Array(numbers) is declared in Line number 13 and initialized in Line number 25 to 27
+				//Additional Info - The enhanced for loop:
+				    /*Also known as the "for-each" loop, is a simplified way to iterate over arrays and collections in Java. 
+					  *It was introduced in Java 5 to make it easier to traverse through elements without using an explicit index.*/
+					
+					    System.out.println("Data Elements of Array(numbers) using Enhanced loop:");
+					    for(int x:numbers) {
+						    System.out.println(x);
+					    }
+					
+				// Accessing all array data elements at once using Arrays.toString() method 
+				    /*In Java, if you want to print all the values of an array at once, 
+				    * you can use Arrays.toString() method from the java.util.Arrays class.
+				    * Syntax : Arrays.toString(ArrayVariableName)*/
+					
+					    System.out.println("Elements of Array(numbers) using Array method: "+Arrays.toString(numbers));
+					    System.out.println("Elements of Array(Levels_Of_Testings) using Array method: "+Arrays.toString(Levels_Of_Testings));
 					
 					
-					
-			// Accessing all array data elements at once using Arrays.deepToString() method 
-					
-					System.out.println("Data Elements of Two Dimension Array(Number) using Array inbuilt method");
-					System.out.println(Arrays.deepToString(Numbers));
+				    /*Additional Info : Explanation - Arrays.toString(numbers)
+					   * Arrays is a class in the java.util package that provides various utility methods for working with arrays.  
+					   * toString(numbers) is a static method of the Arrays class that takes an array as an argument and returns a string representation of that array.*/
 
-	
-	}
-
+		}
 }
